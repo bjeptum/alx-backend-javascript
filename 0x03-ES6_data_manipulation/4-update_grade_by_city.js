15cd155fee81361224e25641412bacd2a8c2168d
bjeptum <brendajeptum@gmail.com>
2024-10-08 19:20:52 +0000

    Combine filter and map

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
