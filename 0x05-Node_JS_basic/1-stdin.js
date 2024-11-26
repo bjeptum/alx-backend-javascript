4caa5f08d7d014db4e6097cb3e2a03e969bc16ab
b_jeptum <brendajeptum@gmail.com>
2024-11-26 23:22:37 +0300

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,23 @@
+// Using Process stdin
+
+const readline = require('readline');
+
+// Read user input from stdin
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Display welcome message and prompt for the user's name
+rl.question('Welcome to Holberton School, what is your name? ', (name) => {
+  // Display the user's name
+  console.log(`Your name is: ${name}`);
+
+  // Display closing message when program ends
+  rl.on('close', () => {
+    console.log('This important software is now closing');
+  });
+
+  // Close readline interface to end program
+  rl.close();
+});
