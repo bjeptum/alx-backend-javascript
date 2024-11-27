47f139b2878e61cbf0a65a44657fcfa0c1267911
b_jeptum <brendajeptum@gmail.com>
2024-11-27 21:14:24 +0300

    A small HTTP server using NodeJS

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+// Create an HTTP server
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!\n');
+});
+
+// listen on port 1245
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
+
+module.exports = app;
